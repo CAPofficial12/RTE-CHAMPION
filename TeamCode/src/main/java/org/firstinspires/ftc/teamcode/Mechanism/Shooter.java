c0a678440cd2907a5527133a74a2b75422ffa66a
Aarav <athakur@dohacollege.com.qa>
2026-01-21 22:57:42 +0300

    Reduced Shooter to atoms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode.Mechanism;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Shooter Tuning")
 public class Shooter{
+
     System_init system_init = new System_init();
 
-    public void SpeedCalc(double distance) {
+    public int SpeedCalc(double distance) {
+        return 5;
 
     }
 }
