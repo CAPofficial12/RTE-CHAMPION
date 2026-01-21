ac4f249ed49c3c74841e01425576517d66e41747
Aarav <athakur@dohacollege.com.qa>
2026-01-21 22:58:36 +0300

    Obelisk pattern works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/Pattern.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.TestCode;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Obelisk Checker")
+public class Pattern extends OpMode {
+    Limelight3A limelight3A;
+    GoBildaPinpointDriver imu;
+    int obelisk;
+
+    @Override
+    public void init(){
+
+        limelight3A  = hardwareMap.get(Limelight3A.class, "Light");
+        limelight3A.pipelineSwitch(7);
+
+        imu = hardwareMap.get(GoBildaPinpointDriver.class, "imu");
+    }
+
+    @Override
+    public void start(){
+        limelight3A.start();
+    }
+
+    @Override
+    public void loop() {
+        LLResult llResult = limelight3A.getLatestResult();
+        if (llResult.isValid()) {
+            obelisk = llResult.getFiducialResults().get(0).getFiducialId();
+            if (obelisk == 21) {
+                telemetry.addLine("GPP");
+            } else if (obelisk == 22) {
+                telemetry.addLine("GPG");
+            } else if (obelisk == 23) {
+                telemetry.addLine("GGP");
+            }
+        }
+        else {
+            telemetry.addLine("MONEY");
+        }
+
+    }
+}
