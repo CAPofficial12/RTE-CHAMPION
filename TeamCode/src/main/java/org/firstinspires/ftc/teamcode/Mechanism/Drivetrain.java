c8448f4cde5a28cc8108b024a3581c9b7c35a99c
Aarav <athakur@dohacollege.com.qa>
2026-02-07 15:19:57 +0300

    Drivetrain Code Updated (Needs to be tested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Drivetrain.java
@@ -4,26 +4,23 @@ package org.firstinspires.ftc.teamcode.Mechanism;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@TeleOp(name = "Driving in my car")
+@TeleOp(name = "Drivetrain go brrr")
 public class Drivetrain extends OpMode {
-    // This declares the four motors needed
-    private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+
+    private TelemetryManager panelsTelemetry;
     DcMotor frontLeftDrive;
     DcMotor frontRightDrive;
     DcMotor backLeftDrive;
     DcMotor backRightDrive;
     boolean Robot = true;
 
-    // This declares the IMU needed to get the current direction the robot is facing
     GoBildaPinpointDriver imu;
 
     @Override
@@ -33,13 +30,9 @@ public class Drivetrain extends OpMode {
         backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
         backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
 
-        // We set the left motors in reverse which is needed for drive trains where the left
-        // motors are opposite to the right ones.
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        // This uses RUN_USING_ENCODER to be more accurate.   If you don't have the encoder
-        // wires, you should remove these
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -62,8 +55,9 @@ public class Drivetrain extends OpMode {
         panelsTelemetry.debug("X", imu.getPosX(DistanceUnit.INCH));
         panelsTelemetry.debug("Y", imu.getPosY(DistanceUnit.INCH));
         panelsTelemetry.debug("Heading", imu.getHeading(AngleUnit.DEGREES));
-        panelsTelemetry.addData("Gamepad 1", gamepad1.toString());
-        panelsTelemetry.addData("Gamepad 2", gamepad2.toString());
+        panelsTelemetry.addData("Gamepad 1 X", gamepad1.left_stick_x);
+        panelsTelemetry.addData("Gamepad 1 Y", gamepad1.left_stick_y);
+        panelsTelemetry.addData("Gamepad 1 Direction", gamepad1.right_stick_x);
 
         panelsTelemetry.update(telemetry);
 
@@ -72,8 +66,7 @@ public class Drivetrain extends OpMode {
         if (gamepad1.circle) {
             imu.resetPosAndIMU();
         }
-        // If you press the left bumper, you get a drive from the point of view of the robot
-        // (much like driving an RC vehicle)
+
         if (Robot) {
             drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
         } else {
@@ -99,15 +92,15 @@ public class Drivetrain extends OpMode {
             backLeftDrive.setPower(-1);
             backRightDrive.setPower(-1);
         }
+
+        imu.update();
     }
 
-    // This routine drives the robot field relative
+
     private void driveFieldRelative(double forward, double right, double rotate) {
-        // First, convert direction being asked to drive to polar coordinates
         double theta = Math.atan2(forward, right);
         double r = Math.hypot(right, forward);
 
-        // Second, rotate angle by the angle the robot is pointing
         theta = AngleUnit.normalizeRadians(theta -
                 imu.getHeading(AngleUnit.RADIANS));
 
@@ -115,38 +108,27 @@ public class Drivetrain extends OpMode {
         double newForward = r * Math.sin(theta);
         double newRight = r * Math.cos(theta);
 
-        // Finally, call the drive method with robot relative forward and right amounts
         drive(newForward, newRight, rotate);
     }
 
-    // Thanks to FTC16072 for sharing this code!!
     public void drive(double forward, double right, double rotate) {
-        // Converts Cartesian coordinate into polar coordinates
-        double power = Math.hypot(forward, right);
-        double theta = Math.atan2(forward, right);
 
-        // Calculated to sin
-        double sin = Math.sin(theta - Math.PI/4);
-        double cos = Math.cos(theta - Math.PI/4);
-        double max = Math.max(Math.abs(cos), Math.abs(sin));
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backRightPower = forward + right - rotate;
+        double backLeftPower = forward - right + rotate;
 
-        // Calculatesd the power each motor needs
-        double frontLeftPower = power * cos/max + rotate;
-        double frontRightPower = power * cos/max - rotate;
-        double backRightPower = power * cos/max - rotate;
-        double backLeftPower = power * cos/max + rotate;
+        double maxSpeed = 1.0;
+        double maxPower;
 
-        // Normlised Power
-        if ((power + Math.abs(rotate)) > 1){
-            frontLeftPower /= max;
-            frontRightPower /= max;
-            backLeftPower /= max;
-            backRightPower /= max;
-        }
+        maxPower = Math.max(Math.abs(frontRightPower), Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
 
-        frontLeftDrive.setPower(frontLeftPower);
-        frontRightDrive.setPower(frontRightPower);
-        backLeftDrive.setPower(backLeftPower);
-        backRightDrive.setPower(backRightPower);
+
+        frontLeftDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
+        frontRightDrive.setPower(maxSpeed * (frontRightPower / maxPower));
+        backLeftDrive.setPower(maxSpeed * (backLeftPower / maxPower));
+        backRightDrive.setPower(maxSpeed * (backRightPower / maxPower));
     }
 }
